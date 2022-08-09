a63aea8260192223b2a259c1d3e2a7db5f45d89d
dberenbaum <dave@iterative.ai>
2022-08-09 16:38:26 -0400

    setup project

---
+++ prep.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from dvc.api import params_show
+
+# Split data into train and test
+lang = sys.argv[1]
+params = params_show(stages=f"prep@{lang}")
+raw_path = f"raw/{lang}"
+with open(raw_path, "r", encoding="utf-8") as f:
+    lines = f.read().split("\n")
+
+# Train split
+train_path = f"train/{lang}"
+os.makedirs("train", exist_ok=True)
+train_lines = int(len(lines) * params["split"])
+with open(train_path, "w", encoding="utf-8") as f:
+    for line in lines[:train_lines]:
+        f.write(line)
+        f.write("\n")
+
+# Test split
+test_path = f"test/{lang}"
+os.makedirs("test", exist_ok=True)
+with open(test_path, "w", encoding="utf-8") as f:
+    for line in lines[train_lines:]:
+        f.write(line)
+        f.write("\n")
